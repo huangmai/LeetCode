52fafe2dd3db980732aa0e3fb29e10b5f6d6576f
Huang Mai <huangmai.personal@gmail.com>
2016-09-09 16:51:39 -0700

    001 AC

---
+++ LeetCode/src/huangmai/leetCode/problems/hash/P0001.java
@@ -0,0 +1,18 @@
+package huangmai.leetCode.problems.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P0001 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> targetMap = new HashMap<>();
+        for(int i = 0; i < nums.length; i ++){
+            if(targetMap.containsKey(nums[i])){
+                return new int[]{targetMap.get(nums[i]), i};
+            }else{
+                targetMap.put(target - nums[i], i);
+            }
+        }
+        return null;
+    }
+}
