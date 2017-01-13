7c753a09e97300e844241b68210af7a63b121031
Huang Mai <huangmai.personal@gmail.com>
2017-01-12 23:15:05 -0800

    0219 resolved

---
+++ LeetCode/src/huangmai/leetCode/problems/hashProblem/P0219.java
@@ -0,0 +1,18 @@
+package huangmai.leetCode.problems.hashProblem;
+
+public class P0219 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(k <= 0){
+            return false;
+        }
+        Map<Integer, Integer> m = new HashMap<>();
+        for(int i = 0, l; i < nums.length; i ++){
+            l = m.getOrDefault(nums[i], -k - 1);
+            if(i - l <= k){
+                return true;
+            }
+            m.put(nums[i], i);
+        }
+        return false;
+    }
+}
