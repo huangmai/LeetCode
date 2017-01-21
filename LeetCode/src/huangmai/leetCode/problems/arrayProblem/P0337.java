faf94fbf82665c60405b8a744232e39f851dfe2f
Huang Mai <huangmai.personal@gmail.com>
2017-01-20 17:42:51 -0800

    0337 resolved

---
+++ LeetCode/src/huangmai/leetCode/problems/arrayProblem/P0337.java
@@ -0,0 +1,23 @@
+package huangmai.leetCode.problems.arrayProblem;
+
+public class P0337 {
+    public int rob(int[] nums) {
+        int a, b, c, t;
+        if(nums.length == 1){
+            return nums[0];
+        }
+        a = b = c = 0;
+        for(int i = 0; i < nums.length - 1; i ++){
+            t = b;
+            b = a;
+            a = Integer.max(t + nums[i], a);
+        }
+        b = 0;
+        for(int i = 1; i < nums.length; i ++){
+            t = b;
+            b = c;
+            c = Integer.max(t + nums[i], c);
+        }
+        return Integer.max(a, c);
+    }
+}
