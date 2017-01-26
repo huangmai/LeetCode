e3c398336f6dc0e00dd20bc9c8519e71f7045225
Huang Mai <huangmai.personal@gmail.com>
2017-01-26 14:39:46 -0800

    0108 resolved recursively. easy

---
+++ LeetCode/src/huangmai/leetCode/problems/treeProblem/P0108.java
@@ -0,0 +1,17 @@
+package huangmai.leetCode.problems.treeProblem;
+
+public class P0108 {
+    private TreeNode toBST(int[] nums, int s, int e){
+        if(s > e){
+            return null;
+        }
+        int m = (s + e >> 1);
+        TreeNode r = new TreeNode(nums[m]);
+        r.left = toBST(nums, s, m - 1);
+        r.right = toBST(nums, m + 1, e);
+        return r;
+    }
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return toBST(nums, 0, nums.length - 1);
+    }
+}
